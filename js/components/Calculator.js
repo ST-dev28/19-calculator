632874690c03fe0329ef5e04360ba7f111ee78e9
SandraT <sndrbltr@gmail.com>
2021-06-04 20:49:17 +0000

    Test #1

--- js/components/Calculator.js
+++ js/components/Calculator.js
@@ -1,37 +1,47 @@
-/*
-3. su JS sugeneruoti HTML:
-- minimum: klavietūra;
-- tobulas: visas objektas
-4. spaudžiant su pele ant mygtukų:
-- jei skaičius ar operatorius, simbolis yra įtraukiamas/parašomas ekrane
-- jei C, tai išvalo ekraną
-- jei < (backspace), tai nutrina vieną simbolį (iš galo)
-- visi kiti nedaro nieko
-*/
+class Calculator {
+    constructor(selector) {
+        this.selector = selector;
 
-const buttonsData = [
-    { title: 'C', color: 'grey' },
-    { title: '+/-', color: 'grey' },
-    { title: '%', color: 'grey' },
-    { title: '/', color: 'dark' },
-    { title: '1' },
-    { title: '2' },
-    { title: '3' },
-    { title: '*', color: 'dark' },
-    { title: '4' },
-    { title: '5' },
-    { title: '6' },
-    { title: '-', color: 'dark' },
-    { title: '7' },
-    { title: '8' },
-    { title: '9' },
-    { title: '+', color: 'dark' },
-    { title: '0' },
-    { title: '.' },
-    { title: '<' },
-    { title: '=', color: 'red' },
-];
+        this.DOM = null;
+        this.buttonsData = [
+            { title: 'C', color: 'grey' },
+            { title: '+/-', color: 'grey' },
+            { title: '%', color: 'grey' },
+            { title: '/', color: 'dark' },
+            { title: '1' },
+            { title: '2' },
+            { title: '3' },
+            { title: '*', color: 'dark' },
+            { title: '4' },
+            { title: '5' },
+            { title: '6' },
+            { title: '-', color: 'dark' },
+            { title: '7' },
+            { title: '8' },
+            { title: '9' },
+            { title: '+', color: 'dark' },
+            { title: '0' },
+            { title: '.' },
+            { title: '<' },
+            { title: '=', color: 'red' },
+        ];
 
-const buttonDOM = document.querySelector('button');
-const inputDOM = document.querySelector('input');
-const spanDOM = document.querySelector('span');
+        this.init();
+    }
+
+    init() {
+        if (!this.isValidSelector() ||
+            !this.findElementBySelector()) {
+            return false;
+        }
+
+        this.render();
+    }
+
+    isValidSelector() {
+        if (typeof this.selector !== 'string' ||
+            this.selector === '') {
+            return false;
+        }
+        return true;
+    }
--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-import { Calculator } from './components/Calculator.js';
+//import { Calculator } from './components/Calculator.js';
 
-new Calculator('body');
+//new Calculator('body');

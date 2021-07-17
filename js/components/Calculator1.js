3eafc3ed66afc091e8503f5f43dcb152166b4d54
SandraT <sndrbltr@gmail.com>
2021-07-17 06:40:39 +0000

    Files renamed

--- js/components/Calculator1.js
+++ js/components/Calculator1.js
@@ -1,137 +1,88 @@
-/*
-3. su JS sugeneruoti HTML:
-- minimum: klavietūra;
-- tobulas: visas objektas
-4. spaudžiant su pele ant mygtukų:
-- jei skaičius ar operatorius, simbolis yra įtraukiamas/parašomas ekrane
-- jei C, tai išvalo ekraną
-- jei < (backspace), tai nutrina vieną simbolį (iš galo)
-- visi kiti nedaro nieko
-*/
 class Calculator {
-    constructor(previousTextElement, currentTextElement) {
-        this.previousTextElement = previousTextElement;
-        this.currentTextElement = currentTextElement;
-        this.clear();
+    constructor(selector) {
+        this.selector = selector;
+
+        this.DOM = null;
+        this.buttonsData = [
+            { title: 'C', color: 'grey' },
+            { title: '+/-', color: 'grey' },
+            { title: '%', color: 'grey' },
+            { title: '/', color: 'dark' },
+            { title: '1' },
+            { title: '2' },
+            { title: '3' },
+            { title: '*', color: 'dark' },
+            { title: '4' },
+            { title: '5' },
+            { title: '6' },
+            { title: '-', color: 'dark' },
+            { title: '7' },
+            { title: '8' },
+            { title: '9' },
+            { title: '+', color: 'dark' },
+            { title: '0' },
+            { title: '.' },
+            { title: '<' },
+            { title: '=', color: 'red' },
+        ];
+
+        this.init();
     }
 
-    clear() {
-        this.currentText = '';
-        this.previousText = '';
-        this.operation = undefined;
-    }
-
-    delete() {
-        this.currentText = this.currentText.toString().slice(0, -1);
-    }
-
-    appendNumber(number) {
-        if (number === '.' && this.currentText.includes('.')) return;      // antro 'kablelio' skaiciuje buti negali
-        this.currentText = this.currentText.toString() + number.toString();
-    }
-
-    chooseOperation(operation) {
-        if (this.currentText === '') return;
-        if (this.previousText !== '') {
-            this.compute();
-
+    init() {
+        if (!this.isValidSelector() ||
+            !this.findElementBySelector()) {
+            return false;
         }
-        this.operation = operation;
-        this.previousText = this.currentText;
-        this.currentText = '';
+
+        this.render();
     }
 
-    compute() {
-        let calculation = 0;
-        const prev = parseFloat(this.previousText);
-        const current = parseFloat(this.currentText);
-        if (isNaN(prev) || isNaN(current)) return;
-        switch (this.operation) {
-            case '+':
-                calculation = prev + current;
-                break;
-            case '-':
-                calculation = prev - current;
-                break;
-            case 'x':
-                calculation = prev * current;
-                break;
-            case '/':
-                calculation = prev / current;
-                break;
-            default:
-                return;
+    isValidSelector() {
+        if (typeof this.selector !== 'string' ||
+            this.selector === '') {
+            return false;
         }
-        this.currentText = calculation;
-        this.operation = undefined;
-        this.previousText = '';
+        return true;
     }
 
-    getDisplayNumber(number) {
-        const stringNumber = number.toString();
-        const integerDigits = parseFloat(stringNumber.split('.')[0]);
-        const decimalDigits = stringNumber.split('.')[1];
-        let integerDisplay = '';
-        if (isNaN(integerDigits)) {
-            integerDisplay = '';
-        } else {
-            integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 });
-        }
-        if (decimalDigits != null) {
-            return `${integerDisplay}.${decimalDigits}`;
-        } else {
-            return integerDisplay;
-        }
+    findElementBySelector() {
+        this.DOM = document.querySelector(this.selector);
+        return !!this.DOM;
     }
 
-    updateDisplay() {
-        this.currentTextElement.innerText = this.getDisplayNumber(this.currentText);
-        if (this.operation != null) {
-            this.previousTextElement.innerText = `${this.getDisplayNumber(this.previousText)} ${this.operation}`;
-            this.currentTextElement.innerText = this.getDisplayNumber(this.currentText);
-        } else {
-            this.previousTextElement.innerText = '';
-        }
+    render() {
+        const HTML = `<div class="calculator">
+                        <div class="screen">
+                            <div class="answer">0</div>
+                            <div class="equation">2+2*2</div>
+                        </div>
+                        <div class="keyboard">
+                            <div class="button">C</div>
+                            <div class="button">+/-</div>
+                            <div class="button">%</div>
+                            <div class="button">/</div>
+                            <div class="button">1</div>
+                            <div class="button">2</div>
+                            <div class="button">3</div>
+                            <div class="button">*</div>
+                            <div class="button">4</div>
+                            <div class="button">5</div>
+                            <div class="button">6</div>
+                            <div class="button">-</div>
+                            <div class="button">7</div>
+                            <div class="button">8</div>
+                            <div class="button">9</div>
+                            <div class="button">+</div>
+                            <div class="button">0</div>
+                            <div class="button">.</div>
+                            <div class="button">&lt;</div>
+                            <div class="button">=</div>
+                        </div>
+                    </div>`;
+
+        this.DOM.insertAdjacentHTML('beforeend', HTML);
     }
 }
 
-
-const numberButtons = document.querySelectorAll('.button.number')
-const operationButtons = document.querySelectorAll('.button.operation')
-const equalsButton = document.querySelector('.button.red')
-const deleteLastButton = document.querySelector('.button.delete')
-const clearButton = document.querySelector('.button.clear')
-const previousTextElement = document.querySelector('.result')
-const currentTextElement = document.querySelector('.action')
-
-const calculator = new Calculator(previousTextElement, currentTextElement);
-
-numberButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        calculator.appendNumber(button.innerText)
-        calculator.updateDisplay()
-    })
-})
-
-operationButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        calculator.chooseOperation(button.innerText)
-        calculator.updateDisplay()
-    })
-})
-
-equalsButton.addEventListener('click', button => {
-    calculator.compute()
-    calculator.updateDisplay()
-})
-
-clearButton.addEventListener('click', button => {
-    calculator.clear()
-    calculator.updateDisplay()
-})
-
-deleteLastButton.addEventListener('click', button => {
-    calculator.delete()
-    calculator.updateDisplay()
-})
-
+export { Calculator }
